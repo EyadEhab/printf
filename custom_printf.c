d8bab1443c017c32cf4f845bacf26ed694bc7d25
EyadEhab <you@example.com>
2023-08-21 05:25:18 -0700

    heaalo

--- custom_printf.c
+++ custom_printf.c
@@ -1,21 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h> /* For putchar function */
 
-/**
- * _printf - Custom printf function
- * @format: Format string with conversion specifiers
- * @...: Variable arguments corresponding to conversion specifiers
- *
- * Return: Number of characters printed (excluding null byte)
- */
 int _printf(const char *format, ...)
 {
     int printed_chars = 0;
     va_list args;
     va_start(args, format);
 
-    for (int i = 0; format[i] != '\0'; i++)
+    int i;
+    for (i = 0; format[i] != '\0'; i++)
     {
         if (format[i] != '%')
         {
@@ -24,35 +18,32 @@ int _printf(const char *format, ...)
         }
         else
         {
-            i++; // Move past the '%'
+            i++; /* Move past the*/ '%'
             switch (format[i])
             {
                 case 'c':
-                    // Handle character argument
+                {
+                    int c = va_arg(args, int);
+                    putchar(c);
+                    printed_chars++;
+                }
+                break;
+                case 's':
+                {
+                    char *s = va_arg(args, char *);
+                    int j;
+                    for (j = 0; s[j] != '\0'; j++)
                     {
-                        int c = va_arg(args, int);
-                        putchar(c);
+                        putchar(s[j]);
                         printed_chars++;
                     }
-                    break;
-                case 's':
-                    // Handle string argument
-                    {
-                        char *s = va_arg(args, char *);
-                        for (int j = 0; s[j] != '\0'; j++)
-                        {
-                            putchar(s[j]);
-                            printed_chars++;
-                        }
-                    }
-                    break;
+                }
+                break;
                 case '%':
-                    // Handle '%' character
                     putchar('%');
                     printed_chars++;
                     break;
                 default:
-                    // Invalid conversion specifier, ignore
                     break;
             }
         }
